951a70f00fd80a8cc60ee62d4d8acaba1ac22d05
jomaway <jonas.malassa@gmail.com>
2022-12-10 15:25:03 +0100

    Completed day9 part2

--- src/solutions/day9.rs
+++ src/solutions/day9.rs
@@ -1,4 +1,4 @@
-use std::{path::Path, collections::HashSet};
+use std::{path::Path, collections::HashSet, process::exit, fmt::Display};
 
 use crate::solutions::get_input_by_line;
 
@@ -18,10 +18,6 @@ pub fn run() {
     println!("Result of task2 is {}",res2);
 }
 
-
-
-
-
 #[derive(Debug, Default, PartialEq, Eq, Hash, Clone, Copy)]
 struct Position {
     x: i32,
@@ -40,6 +36,12 @@ impl Position {
     }
     fn one_down(&mut self) {
         self.y -= 1;
+    }
+}
+
+impl Display for Position {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}/{})",self.x, self.y)
     }
 }
 
@@ -78,8 +80,42 @@ pub fn task1(path: &Path) -> u32 {
 }
 
 pub fn task2(path: &Path) -> u32 {
-    unimplemented!();
+    let instructions = get_input_by_line(path);
+    let mut rope : Vec<Position> = vec![Position::default(); 10];
+    let mut history: HashSet<Position> = HashSet::new();
+    history.insert(rope[9]);
+    
+    for line in instructions {
+        let (direction, amount) = line.split_once(' ').expect("Couldn't parse input line");
+        let amount = amount.parse().unwrap();
+        for _ in 0..amount {
+            let mut last = rope[0].clone();
+            match direction {
+                "L" => rope[0].one_left(),
+                "R" => rope[0].one_right(),
+                "U" => rope[0].one_up(),
+                "D" => rope[0].one_down(),
+                _ => unreachable!(),
+            }
+
+            for knot in 1..rope.len() {
+                let delta_x = rope[knot-1].x - rope[knot].x;
+                let delta_y = rope[knot-1].y - rope[knot].y;
+                let dis = delta_x.pow(2) + delta_y.pow(2);
+            
+                if dis > 2 {
+                    rope[knot].x += delta_x.signum();
+                    rope[knot].y += delta_y.signum();
+                }
+
+            }
+            println!("Tail at: {}", rope[9]);
+            history.insert(rope[9]);
+        }
+    }
+    history.len() as u32
 }
+
 
 mod tests { 
     use std::path::Path;
@@ -97,7 +133,7 @@ mod tests {
     
     #[test]
     fn test_task2(){
-        let path_str : String = format!("src/inputs/day{}-e.txt",DAY);
+        let path_str : String = format!("src/inputs/day{}-e2.txt",DAY);
         let path = Path::new(&path_str);
         let expected_result: u32 = 36;
 
